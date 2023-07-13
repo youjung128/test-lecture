2c46861c8c4c9a41f4ace765015e6b9a48c41fc6
youjung <kelly000128@naver.com>
2023-07-13 18:13:11 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//모듈
+const express = require("express");
+const app = express();
+
+const PORT = 3005;
+
+//라우팅
+const home = require("./src/routes/home");
+
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+
+app.use("/", home); // use -> 미들웨어 등록해주는 메소드
+
+//보내주기
+module.exports = app;
+
---
+++ app/bin/www.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const app = require("../app");
+const PORT = 3005;
+
+app.listen(PORT, () => {
+    console.log("서버 가동"); // 여기서 실행해야 함
+});
